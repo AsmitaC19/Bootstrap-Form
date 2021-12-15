0e9ecab57fbf7bde66d71b0e36e70c2de95cbba5
AsmitaC19 <88437259+AsmitaC19@users.noreply.github.com>
2021-12-14 22:12:22 -0600

    Create app.component.ts

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component, ViewChild, TemplateRef} from '@angular/core';
+import {BsModalRef, ModalDirective,BsModalService} from 'ngx-bootstrap/modal';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent 
+{
+  title = 'BootstrapForm';
+}
